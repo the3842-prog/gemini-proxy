0c7b2796eebd7c5046166bf6ab225040f0e4664e
the3842-prog <the3842@naver.com>
2026-09-20 19:36:53 +0900

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -1,6 +1,6 @@
 export default async function handler(req, res) {
   // 웹사이트 통신 허용 설정 (CORS)
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://www.t-happy.co.kr');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
